01b05e7a75547de7d554addfb367aea43fe61f3e
Ubuntu <ubuntu@ip-172-31-29-192.ec2.internal>
2026-02-01 03:28:12 +0000

    cambios

---
+++ resources/js/app.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+function App() {
+    return <h1>Hello from React inside web-bertha!</h1>;
+}
+
+createRoot(document.getElementById('app')).render(<App />);
+
